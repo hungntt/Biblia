ca5fd436f30f6e0664680783809ca1f50954c240
Nguyen Truong Thanh Hung <hungntt@zigexn.vn>
2019-12-17 16:32:07 +0700

    Chapter 7: Fix RuboCop style for macros

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ApplicationRecord
-  before_save { email.downcase! }
-  validates :name, presence: true, length: {maximum: 50}
+  # Constants
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+
+  # Validation macros
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  # Callback
+  before_save { email.downcase! }
+
+  # Other macros
   has_secure_password
-  validates :password, presence: true, length: {minimum: 6}
 end
