b8588598cd8485ad2bb6cef4d5b545f43cae48e7
Nguyen Truong Thanh Hung <hungntt@zigexn.vn>
2019-12-18 13:40:56 +0700

    Chapter 7: Fix render and use double quote

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to my app!"
       redirect_to @user
     else
-      render "new"
+      render :new
     end
   end
 
