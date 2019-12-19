b3225ab7472af1fcd6773d7b7b42ca67443ebef1
Nguyen Truong Thanh Hung <hungntt@zigexn.vn>
2019-12-19 10:57:01 +0700

    Chapter 9: Submit Advanced Login before feedback

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,12 +6,25 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors, with: :threads)
-  # Returns true if a test user is logged in.
-  def logged_in?
-    session[:user_id].present?
-  end
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include ApplicationHelper
   # Add more helper methods to be used by all tests here...
+  # Returns true if a test user is logged in.
+  def logged_in?
+    session[:user_id].present?
+  end
+
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  # Log in as a particular user.
+  def log_in_as(user, password: "password", remember_me: "1")
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
