153eae25ef244a5e190da234df6a851686ddb771
Nguyen Truong Thanh Hung <hungntt@zigexn.vn>
2019-12-19 13:49:09 +0700

    Chapter 10: Init edit/update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_param)
+    @user = User.new(user_params)
     if @user.save
       log_in @user
       flash[:success] = "Welcome to my app!"
@@ -18,9 +18,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render :edit
+    end
+  end
+
   private
 
-  def user_param
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
