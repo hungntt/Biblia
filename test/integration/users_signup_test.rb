d4b6685a571d9103a32beb4239ed5bff12ba67dc
Nguyen Truong Thanh Hung <hungntt@zigexn.vn>
2019-12-20 13:03:39 +0700

    Chapter 9: Double quote

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,7 +11,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                 password: "foo",
                 password_confirmation: "bar" } }
     end
-    assert_template 'users/new'
+    assert_template "users/new"
   end
 
   test "valid signup information" do
@@ -19,8 +19,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_difference "User.count", 1 do
       post users_path, params:
           { user:
-               {
-                   name: "Example User",
+               {   name: "Example User",
                    email: "user@example.com",
                    password: "password",
                    password_confirmation: "password" } }
