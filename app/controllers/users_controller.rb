0943db87ab80c83a205076200a77153827a2c2d3
Nguyen Truong Thanh Hung <hungntt@zigexn.vn>
2019-12-20 13:55:30 +0700

    Chapter 9: Change the order of before_action to reduce queries

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
-  before_action :load_user, except: %i[new index]
   before_action :logged_in_user, except: %i[new show create]
+  before_action :load_user, except: %i[new index]
   before_action :correct_user, only: %i[edit update]
   before_action :admin_user, only: %i[destroy]
-
 
   def new
     @user = User.new
