70524090d2e296baebf0de191937ffe6a7bf0f2f
Nguyen Truong Thanh Hung <hungntt@zigexn.vn>
2019-12-27 08:28:29 +0700

    Chapter 13: Improve SCSS, add seeds

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,6 @@
 module UsersHelper
-  def gravatar_for(user, options = {size: 100})
+  def gravatar_for(user, size)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
